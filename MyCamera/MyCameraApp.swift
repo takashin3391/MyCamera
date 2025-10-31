1b52df14fc6a03cb7d23bf7ced57c4cd4c092451
Takayama <tkym1982@gmail.com>
2025-10-31 13:30:02 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by Takayama on 2025/10/31.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
