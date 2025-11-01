3e24afb1e692182320359fc7f321728ef90663d1
Takayama <tkym1982@gmail.com>
2025-11-01 21:39:42 +0900

    諸々変更

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -6,16 +6,92 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State var captureImage: UIImage? = nil
+    
+    @State var isShowSheet = false
+    
+    @State var photoPickerSelectedImage: PhotosPickerItem? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            if let captureImage {
+                Image(uiImage: captureImage)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Spacer()
+            
+            Button {
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    print("カメラは利用できます")
+                    isShowSheet.toggle()
+                }
+                else {
+                    print("カメラは利用できません")
+                }
+            }label: {
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }
+            .padding()
+            .sheet(isPresented: $isShowSheet) {
+                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+            }
+            
+            PhotosPicker(selection: $photoPickerSelectedImage, matching: .images, preferredItemEncoding: .automatic, photoLibrary: .shared()){
+                Text("フォトライブラリから選択する。")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .padding()
+            }
+            .onChange(of: photoPickerSelectedImage) { photoPickerItem in
+                if let photoPickerItem {
+                    photoPickerItem.loadTransferable(type: Data.self) { result in
+                                                                          switch result {
+                    case .success(let data):
+                        if let data {
+                            captureImage = UIImage(data: data)
+                        }
+                    case .failure:
+                        return
+                    }
+                    }
+                }
+            }
+            
+            if let captureImage {
+                // captureImageから共有する画像を生成する
+                let shareImage = Image(uiImage: captureImage)
+                // 共有シート
+                ShareLink(item: shareImage, subject: nil, message: nil,
+                          preview: SharePreview("Photo", image: shareImage)) {
+                    // テキスト表示
+                    Text("SNSに投稿する")
+                    // 横幅いっぱい
+                        .frame(maxWidth: .infinity)
+                    // 高さ50ポイント指定
+                        .frame(height: 50)
+                    // 背景を青色に指定
+                        .background(Color.blue)
+                    // 文字色を白色に指定
+                        .foregroundColor(Color.white)
+                    // 上下左右に余白を追加
+                        .padding()
+                } // ShareLinkここまで
+            } // アンラップここまで
         }
-        .padding()
     }
 }
 
---
+++ MyCamera/ImagePickerView.swift
@@ -0,0 +1,48 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by Takayama on 2025/10/31.
+//
+
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    @Binding var isShowSheet: Bool
+    
+    @Binding var captureImage: UIImage?
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePickerView
+        
+        init(_ parent: ImagePickerView){
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.captureImage = originalImage
+            }
+            parent.isShowSheet.toggle()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.isShowSheet.toggle()
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let myImagePickerController = UIImagePickerController()
+        myImagePickerController.sourceType = .camera
+        myImagePickerController.delegate = context.coordinator
+        return myImagePickerController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        //処理なし
+    }
+}
